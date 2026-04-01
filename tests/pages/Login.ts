d11f1d5311b19d02515797e88f4ee5a1fc879bcf
Veselin Micunovic <veselinmicunovic@gmail.com>
2026-04-01 08:51:44 +0200

    add login fix

--- tests/pages/Login.ts
+++ tests/pages/Login.ts
@@ -1,13 +1,30 @@
 import { Page, expect } from "@playwright/test";
 
-export async function login(page: Page, email: string, password: string) {
-    await page.locator('[data-test-id="login-email-input"]').fill(email);
-    await page.locator('[data-test-id="login-password-input"]').fill(password);
-    await page.locator('[data-test-id="login-submit-button"]').click();
-}
+export class LoginPage {
 
-export async function verifySuccessfulLogin(page: Page) {
-    // After successful login, user should be redirected to home page
-    await expect(page).toHaveURL('/', { timeout: 10000 });
-}
+    readonly page: Page;
+    private readonly emailInput = '[data-test-id="login-email-input"]';
+    private readonly passInput = '[data-test-id="login-password-input"]';
+    private readonly clickLoginButton = '[data-test-id="login-submit-button"]';
 
+    constructor(page: Page) {
+        this.page = page;
+
+    }
+
+    async login(email: string, password: string) {
+        await this.page.locator(this.emailInput).fill(email);
+        await this.page.locator(this.passInput).fill(password);
+        await this.page.locator(this.clickLoginButton).click();
+    }
+
+    async clickLogin() {
+        await this.page.locator(this.clickLoginButton).click();
+    }
+
+
+    async verifySuccessfulLogin(page: Page) {
+        // After successful login, user should be redirected to home page
+        await expect(page).toHaveURL('/', { timeout: 10000 });
+    }
+}
