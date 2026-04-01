f1bc42d0f41c20e3b84cb14d3e49b5127655fd8d
Veselin Micunovic <veselinmicunovic@gmail.com>
2026-04-01 15:09:59 +0200

    login test improvment

--- tests/specs/login-flow.spec.ts
+++ tests/specs/login-flow.spec.ts
@@ -1,65 +1,61 @@
-import { test, expect } from '@playwright/test';
-import { Base } from '../pages/Base';
+import { test as base, expect } from '@playwright/test';
 import { Login } from '../pages/Login';
-import path from 'path'
-import fs from 'fs'
+import path from 'path';
+import fs from 'fs';
 
 const loginDataFile = path.resolve(__dirname, '../../playwright/.auth/loginData.json');
 const loginData = JSON.parse(fs.readFileSync(loginDataFile, 'utf-8')) as {
     email: string,
     pass: string
-}
+};
 
-const validationTestData = [
-    {
-        errorEmailMessage: 'Please enter a valid email address.',
-        errorPasswordMessage: 'Password is required.',
+const testData = {
+    emptyFields: {
+        errorEmail: 'Please enter a valid email address.',
+        errorPassword: 'Password is required.',
     },
-    {
-        email: loginData.email,
+    wrongPassword: {
         password: '1234',
-        errorPasswordMessage: 'Incorrect username or password.',
+        errorMessage: 'Incorrect username or password.',
     },
-    {
-        email: "testvesko@gmailll.com",
-        password: loginData.pass,
-        errorEmailMessage: 'User does not exist.',
+    invalidEmail: {
+        email: 'testvesko@gmailll.com',
+        errorMessage: 'User does not exist.',
     }
+};
 
-];
+const test = base.extend<{ loginPage: Login }>({
+    loginPage: async ({ page }, use) => {
+        const loginPage = new Login(page);
+        await page.goto('/login');
+        await use(loginPage);
+    }
+});
 
 test.describe('Login Flow', () => {
-    test.describe.configure({ mode: 'serial' });
     test.use({ storageState: { cookies: [], origins: [] } });
-    let loginPage: Login;
 
-    test.beforeEach(async ({ page }) => {
-        loginPage = new Login(page);
-        await page.goto('/login');
+    test('Validation errors - empty email and password fields', async ({ page, loginPage }) => {
+        await loginPage.clikcLogin();
+
+        await expect(page.getByText(testData.emptyFields.errorEmail, { exact: true })).toBeVisible();
+        await expect(page.getByText(testData.emptyFields.errorPassword, { exact: true })).toBeVisible();
     });
 
-    test('Validation errors - empty email and password fields', async ({ page }) => {
-        await page.locator('[data-test-id="login-submit-button"]').click();
+    test('Wrong password - shows error message', async ({ page, loginPage }) => {
+        await loginPage.loginFlow(loginData.email, testData.wrongPassword.password);
 
-        await expect(page.getByText(validationTestData[0].errorEmailMessage!, { exact: true })).toBeVisible();
-        await expect(page.getByText(validationTestData[0].errorPasswordMessage!, { exact: true })).toBeVisible();
+        await expect(page.getByText(testData.wrongPassword.errorMessage, { exact: true })).toBeVisible();
     });
 
-    test('Wrong password - shows error message', async ({ page }) => {
-        await loginPage.loginFlow(loginData.email, validationTestData[1].password!);
+    test('Invalid email - shows error message', async ({ page, loginPage }) => {
+        await loginPage.loginFlow(testData.invalidEmail.email, loginData.pass);
 
-        await expect(page.getByText(validationTestData[1].errorPasswordMessage!, { exact: true })).toBeVisible();
+        await expect(page.getByText(testData.invalidEmail.errorMessage, { exact: true })).toBeVisible();
     });
 
-     test('Invalid email - shows error message', async ({ page }) => {
-        await loginPage.loginFlow(validationTestData[2].email!, loginData.pass);
-
-        await expect(page.getByText(validationTestData[2].errorEmailMessage!, { exact: true })).toBeVisible();
-    });
-
-    test('Happy flow - successful login', async ({ page }) => {
+    test('Happy flow - successful login', async ({ loginPage }) => {
         await loginPage.loginFlow(loginData.email, loginData.pass);
         await loginPage.verifySuccessfulLogin();
     });
-
 });
