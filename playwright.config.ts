ad99280732e44ac57fe44447c6d83d61760b1c62
Veselin Micunovic <veselinmicunovic@gmail.com>
2026-04-17 15:00:45 +0200

    edidet workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,10 +26,11 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
     ['list'],
+    ['github'],
     ['junit', { outputFile: 'reports-e2e/junit.xml' }],
     ['html', { outputFolder: 'reports-e2e/html', open: 'never' }],
   ],
